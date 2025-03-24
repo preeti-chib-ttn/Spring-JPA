5eb5cc01cf2a3b798000e256072ac4139cfdb8fd
Preeti Chib <preeti.chib@tothenew.com>
2025-03-24 07:36:29 +0530

    few changes

--- src/main/java/com/bootcamp/jpabootcamp/entity/Author.java
+++ src/main/java/com/bootcamp/jpabootcamp/entity/Author.java
@@ -1,15 +1,46 @@
 package com.bootcamp.jpabootcamp.entity;
 
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 
     @Embedded
     Address address;
+
+    @ElementCollection
+    private List<String> subjects;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "one_to_one_book_id")
+    private Book oneToOneBook;
+
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> oneToManyBooks;
+
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "author_book_many_to_many",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private List<Book> manyToManyBooks;
+
+
 }
+
+
--- src/main/java/com/bootcamp/jpabootcamp/entity/Book.java
+++ src/main/java/com/bootcamp/jpabootcamp/entity/Book.java
@@ -1,8 +1,27 @@
 package com.bootcamp.jpabootcamp.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String bookName;
+
+    // Many-to-One Relationship
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    // Many-to-Many Relationship
+    @ManyToMany(mappedBy = "manyToManyBooks")
+    private List<Author> authors;
 }
